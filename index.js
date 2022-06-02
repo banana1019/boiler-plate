631f971cc12ca4d955cad0dcd9cd9984c38010a0
banana1019 <jyho930@gmail.com>
2022-06-02 17:09:42 +0900

    backend 구현 완료

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 // body-parser: 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해주는 거
@@ -27,7 +28,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/register", (req, res) => {
+app.post("api/users/register", (req, res) => {
   // 회원가입할 때 필요한 정보들을 client 에서 가져오면
   // 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
@@ -40,7 +41,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -67,6 +68,29 @@ app.post("/login", (req, res) => {
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해왔다는 얘기는 Authentication이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
   });
 });
