8713493040c53fe00d0586b44a087b96ce248db9
banana1019 <jyho930@gmail.com>
2022-06-02 15:12:51 +0900

    #12 토큰 생성 with jsonwebtoken(JWT)

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express"); // express 모듈을 가져온다.
 const app = express(); // 위 function을 이용해서 새로운 express app을 만든다.
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { User } = require("./models/User");
 
 // body-parser: 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해주는 거
+
 // application/x-www-form-urlencoded 데이터를 분석해서 가져올 수 있게 해주기 위해서
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //
+
 // application/json 타입으로 된 데이터를 분석해서 가져올 수 있게 해주기 위해서
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // mongoose 모듈을 가져와서 어플리케이션과 mongoDB를 연결한다.
 const mongoose = require("mongoose");
@@ -32,6 +36,37 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 비밀번호까지 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, ...
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
