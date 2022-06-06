482f1434e37544d8cc4e87c6202cbb1351aa838c
banana1019 <jyho930@gmail.com>
2022-06-06 23:53:22 +0900

    회원가입 페이지 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER } from "./types";
 
 export function loginUser(dataToSubmit) {
   const request = axios
@@ -8,6 +8,17 @@ export function loginUser(dataToSubmit) {
 
   return {
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post("/api/users/register", dataToSubmit)
+    .then((response) => response.data); // 서버에서 받은 데이터를 request에 저장한다.
+
+  return {
+    type: REGISTER_USER,
     payload: request,
   };
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { loginUser } from "../../../_actions/user_action";
+import { registerUser } from "../../../_actions/user_action";
 import { useNavigate } from "react-router-dom";
 
 function RegisterPage(props) {
@@ -8,14 +8,24 @@ function RegisterPage(props) {
   let navigate = useNavigate();
 
   const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
   const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
   };
 
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
   };
 
   const onSubmitHandler = (event) => {
@@ -24,17 +34,22 @@ function RegisterPage(props) {
     // console.log("Email", Email);
     // console.log("Password", Password);
 
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호가 일치하지 않습니다.");
+    }
+
     let body = {
       email: Email,
+      name: Name,
       password: Password,
     };
 
-    dispatch(loginUser(body)).then((response) => {
-      if (response.payload.loginSuccess) {
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
         //props.history.push("/");
-        navigate("/");
+        navigate("/login");
       } else {
-        alert("Error");
+        alert("회원가입 실패");
       }
     });
   };
@@ -55,14 +70,22 @@ function RegisterPage(props) {
       >
         <label>Email</label>
         <input type="email" value={Email} onChange={onEmailHandler}></input>
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler}></input>
         <label>Password</label>
         <input
           type="password"
           value={Password}
           onChange={onPasswordHandler}
         ></input>
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        ></input>
         <br></br>
-        <button type="submit">Login</button>
+        <button type="submit">가입하기</button>
       </form>
     </div>
   );
